27b37a48882fefc3b86180efc799f46d3064a7d0
Allysson Santos <68858220+Allyssonantos@users.noreply.github.com>
2024-03-19 23:10:59 -0300

    01

--- pythonProject/bonus_sarial_condição.py
+++ pythonProject/bonus_sarial_condição.py
@@ -1,10 +1,10 @@
 anos = int(input('Quanto tempo na empresa? '))
 salario = int(input('Qual o seu salario? '))
-if anos <= 1:
+if anos <= 1: # se vc trabalhou menos de 1 ano ou um ano
     print('Você tem um aumento de 0%')
 else:
     if anos >= 1 and anos < 10:
-        salario_porcetagem = salario * (10/100)
+        salario_porcetagem = salario * (10/100) #
         print('O aumento de 10% é R$', salario_porcetagem + salario)
 
     else:
