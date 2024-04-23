dfedfdd556cd9224039dfba7e3dc9d04741c87e1
Allysson Santos <68858220+Allyssonantos@users.noreply.github.com>
2024-04-23 19:34:29 -0300

    05

--- pythonProject/cardapio_digital.py
+++ pythonProject/cardapio_digital.py
@@ -1,35 +1,27 @@
 # Definindo o cardápio
-cardapio = {
-    "1": {"nome": "Água", "preco": 1},
-    "2": {"nome": "Coxinha", "preco": 6.9},
-    "3": {"nome": "Hambúrguer", "preco": 5.99},
-    "4": {"nome": "Batata Frita", "preco": 10.99},
-    "5": {"nome": "Refrigerante", "preco": 7.99},
-    "6": {"nome": "Suco de Laranja", "preco": 1.9},
-    "7": {"nome": "Sorvete", "preco": 10.99},
-    "8": {"nome": "Café", "preco": 4.99}
-}
+print('0: hambuguer - R$ 17,99')
+print('1: cheeseburguer - R$ 13,99')
+print('2: hotdog - R$ - 6,99')
+
+opcao = int(input("Digite a opção desejada: "))
+while opcao >= 0 and opcao <= 2:
+    if opcao == 0:
+        qnt = int(input('Digite a quantidade de hambugueres: '))
+        total = 17.99 * qnt
+        print(f'Você pediu {qnt} hamburgures. O total é R$ {total}')
+
+    elif opcao == 1:
+
+        qnt = int(input('Digite a quantidade de cheeseburguer: '))
+        total = 13.99 * qnt
+        print(f'Você pediu {qnt} cheeseburguer. O total é R$ {total}')
+
+    elif opcao == 2:
+        qnt = int(input('Digite a quantidade de hotdog: '))
+        total = 6.99 * qnt
+        print(f'Você pediu {qnt} hotdog. O total é R$ {total}')
+
+    else:
+        print("opcão invalida")
 
 
-# Função para exibir o cardápio
-def exibir_cardapio():
-    print("=== Cardápio ===")
-    for codigo, item in cardapio.items():
-        print(f"{codigo}: {item['nome']} - R${item['preco']:.2f}")
-
-
-# Função principal
-def main():
-    exibir_cardapio()
-    opcao = input("Digite o número do item desejado: ")
-    if opcao not in cardapio:
-        print("Opção inválida!")
-        return
-
-    quantidade = int(input("Digite a quantidade desejada: "))
-    total = cardapio[opcao]["preco"] * quantidade
-    print(f"Total a pagar: R${total:.2f}")
-
-
-if __name__ == "__main__":
-    main()
